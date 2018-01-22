3424b2d0c6bdb974ff01572e5ea086102bef8a86
fan <fan@hudongpai.com>
2018-01-22 14:50:42 +0800

    任务三：原声js轮播图80%

--- html-css/js/mission3_js_slide_show.js
+++ html-css/js/mission3_js_slide_show.js
@@ -1,13 +1,20 @@
-var imags = document.querySelector(".imags");
+// var imags = document.querySelector(".imags");
+var imagss = document.getElementsByClassName("imags");
+var imags = imagss[0];
 var wrapId = document.getElementById("wrapId");
 var wrap = document.getElementsByClassName("wrap");
 var leftArrow = document.getElementById("leftArrow");
 var rightArrow = document.getElementById("rightArrow");
+var buttonss = document.getElementsByClassName("buttons");
+var buttons = buttonss[0];
+var buttonsEle = buttons.children;
+var buttonLength = buttonsEle.length;
 var imagsEle = imags.children;
 var imagIndex = 0;
 var length = imagsEle.length;
 var changeTimeFlag = true;
 var mouseleaveFlag = false;
+var autoSlideTimeFlag = true;
 
 
 function clickSlide (){
@@ -38,13 +45,7 @@ function leftSlide(){
 	}
 }
 
-// var timer = null;
-// function autoSlide (){
-// 	timer = setInterval(function(){clickSlide();}, 1000);
-// }
-// autoSlide();
-
-var time = 1288;
+var time = 2222;
 var timer = null;
 function autoSlideNeedTime (time){
 	console.log("slideShow");
@@ -71,25 +72,104 @@ function autoSlideNeedTime (time){
 // autoChangeSlide();
 // autoSlideNeedTime(time);
 
+function clearAllInterval(){
+	console.log("clearAllInterval");
+	// clearInterval(changeTimer);
+	clearInterval(timer);
+	clearInterval(leftAutoSlideTimer);
+	clearInterval(rightAutoSlideTimer);
+}
+
+//滑动函数
+var index = 0;
+function autoSlide(){
+	console.info("index:"+index);
+	var begin=imags.offsetLeft;
+	var end=index*wrapId.offsetWidth*(-1);
+	var change=end-begin;
+	var autoSlideTimer;
+	var ingT=0;
+	var maxT=30;
+	// 限制滑动完后才能继续滑动
+	autoSlideTimeFlag=false;
+	//按钮
+	clearButtons();
+	changeButtons(index);
+	//开始滑动
+	autoSlideTimer=setInterval(function(){
+		ingT++;
+		if (ingT>maxT) {
+			clearInterval(autoSlideTimer);
+			if (index==length-1) {
+				imags.style.left=0;
+				index=0;
+			}
+			autoSlideTimeFlag=true;
+		}else{
+			imags.style.left=change/maxT*ingT+begin+"px";
+		}
+	},17);
+}
+
+// 改变按钮状态
+function changeButtons(index){
+	//index:
+	if (index==buttonLength) {
+		buttonsEle[0].className="selected";
+	}else{
+		buttonsEle[index].className="selected";
+	}
+	
+}
+//清除页面所有按钮状态颜色
+function clearButtons(){ 
+	for(var i=0;i<buttonLength;i++){ 
+		buttonsEle[i].className=""; 
+	} 
+} 
+
+
+// 右滑动
+var rightAutoSlideTimer;
+function rightAutoSlide(){
+	if (autoSlideTimeFlag) {
+		index++;
+		autoSlide();
+	}
+}
+
+// 左滑动
+var leftAutoSlideTimer;
+function leftAutoSlide(){
+	if (autoSlideTimeFlag) {
+		index--;
+		if (index<0) {
+			index=length-2;
+			imags.style.left=(index+1)*wrapId.offsetWidth*(-1)+"px";
+		}
+		autoSlide();
+	}
+}
+
+//事件
+rightAutoSlideTimer=setInterval(function(){
+	rightAutoSlide();
+},time);
 leftArrow.onclick = function(){
 	if (length < 0) {
 		alert("no imag to slide show");
 	}else{
 		// leftSlide();
+		leftAutoSlide();
 	}
 }
-rightArrow.onclick =function(){
+rightArrow.onclick = function(){
 	if (length < 0) {
 		alert("no imag to slide show");
 	}else{
 		// clickSlide();
+		rightAutoSlide();
 	}
-}
-
-function clearAllInterval(){
-	console.log("clearAllInterval");
-	// clearInterval(changeTimer);
-	clearInterval(timer);
 }
 wrapId.onmouseover = function(){
 	clearAllInterval();
@@ -99,95 +179,26 @@ wrapId.onmouseout = function(){
 	console.log("onmouseleave");
 	// autoChangeSlide();
 	// autoSlideNeedTime(time);
+	if (autoSlideTimeFlag) {
+		rightAutoSlideTimer=setInterval(function(){
+			rightAutoSlide();
+		},time);
+	}
 }
 
 var stopButton = document.getElementById("stopButton");
 stopButton.onmousedown = function(event){
-	console.log(event.button);
+	// console.log(event.button);
 	clearAllInterval();
 }
-
-
-//滑动函数
-var index = 0;
-function autoSlide(){
-	var begin=imags.offsetLeft;
-	var end=index*wrapId.offsetWidth*(-1);
-	var change=end-begin;
-	var autoSlideTimer;
-	var ingT=0;
-	var maxT=30;
-	autoSlideTimer=setInterval(function(){
-		ingT++;
-		if (ingT>maxT) {
-			clearInterval(autoSlideTimer);
-			if (index==length-1) {
-				imags.style.left=0;
-				index=0;
-			}
-		}else{
-			imags.style.left=change/maxT*ingT+begin+"px";
+for (var i = 0;i<buttonLength;i++) {
+	buttonsEle[i].onclick = function(){
+		if (autoSlideTimeFlag) {
+			index = this.innerText - 1;
+			autoSlide();
 		}
-	},17);
-}
-
-// var rightAutoSlideTimer;
-// function rightAutoSlide(){
-// 	index++;
-// 	autoSlide();
-// }
-// rightAutoSlideTimer=setInterval(function(){
-// 	rightAutoSlide();
-// },1000);
-
-var leftAutoSlideTimer;
-function leftAutoSlide(){
-	index--;
-	if (index<0) {
-		index=length-2;
-		imags.style.left=(index+1)*wrapId.offsetWidth*(-1)+"px";
-	}
-	autoSlide();
-}
-leftArrow.onclick = function(){
-	if (length < 0) {
-		alert("no imag to slide show");
-	}else{
-		// leftSlide();
-		leftAutoSlide();
 	}
 }
-leftAutoSlideTimer=setInterval(function(){
-	leftAutoSlide();
-},1000);
-
-// function AutoGo(){
-//             var start=inner.offsetLeft;//获取移动块当前的left的开始坐标
-//             var end=index*Distance*(-1);//获取移动块移动结束的坐标。
-//             //计算公式即当移动到第三张图片时，图片下标为2乘以图片的宽度就是块的left值。
-//             var change=end-start;//偏移量
-
-//             var timer;//用计时器为图片添加动画效果
-//             var t=0;
-//             var maxT=30;
-//             clear();//先把按钮状态清除,再让对应按钮改变状态
-//             if(index==spanList.length){
-//                 spanList[0].className="selected";
-//             }else{
-//                 spanList[index].className="selected";
-//             }
-//             clearInterval(timer);//开启计时器前先把之前的清
-//             timer=setInterval(function(){
-//                 t++;
-//                 if(t>=maxT){//当图片到达终点停止计时器
-//                     clearInterval(timer);
-//                     clickFlag=true;//当图片到达终点才能切换
-//                 }
-//                 inner.style.left=change/maxT*t+start+"px";//每个17毫秒让块移动
-//                 if(index==spanList.length&&t>=maxT){
-//                     inner.style.left=0;
-//                     index=0;
-//                     //当图片到最后一张时把它瞬间切换回第一张，由于都同一张图片不会影响效果
-//                 }
-//             },17);
-//         }
+// leftAutoSlideTimer=setInterval(function(){
+// 	leftAutoSlide();
+// },time);
