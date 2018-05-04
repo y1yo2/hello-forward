cc8a03e0f7e26034f5c69f886673399de66d22b2
zhuhuiting <2503453269@qq.com>
2018-05-04 18:35:17 +0800

    礼物说
    小程序

---
+++ lws/src/pages/giveGift/index.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="giveGift">
+  首页
+</div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    name: "giveGift",
+    data() {
+      return {}
+    },
+    methods: {}
+  }
+</script>
+
+<style>
+
+</style>
